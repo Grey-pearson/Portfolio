e3fb981706c49bc469f672e0914fb70c6006b452
Grey-pearson <72120360+Grey-pearson@users.noreply.github.com>
2023-04-24 22:35:10 -0500

    setting up pages and components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#483D8B',
+    },
+    secondary: {
+      main: '#DBEBEB',
+    },
+  },
+});
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/aboutMe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function AboutMe() {
+    return (
+        <Box>about me</Box>
+    );
+}
+
+export default AboutMe;
